692b7ff943868c2ce53475cade644f2010da677d
Matthew Stanley <matthew.stanley35t@gmail.com>
2022-10-14 14:38:04 -0600

    did some refactoring for_loops.js, break_and_continue.js, and while.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,25 +1,25 @@
+(function(){
 "use strict";
 
-// let numberToSkip = "a number";
-// let useableNumber = 0;
-//
-// while (numberToSkip != useableNumber){
-//     numberToSkip = prompt("Please enter an ODD number between 1 and 50");
-//     if (numberToSkip % 2 !== 0 && numberToSkip > 0 && numberToSkip < 50){
-//        useableNumber = numberToSkip
-//         break;
-//     }
-//     console.log(numberToSkip);
-// }
-//
-// for(var i = 1; i < 50; i++){
-//     if(i === parseFloat(numberToSkip)){
-//         console.log("Yikes! Skipping number: " + numberToSkip);
-//         continue;
-//     }
-//     if(i % 2 === 0){
-//         continue;
-//     }
-//
-//     console.log("Here is an odd number: " + i);
-// }
+let numberToSkip = 0;
+
+while (numberToSkip !== undefined){
+    numberToSkip = prompt("Please enter an ODD number between 1 and 50");
+    if (numberToSkip % 2 !== 0 && numberToSkip > 0 && numberToSkip < 50){
+        console.log("Number to skip is: " + numberToSkip);
+        break;
+    }
+    console.log(numberToSkip); //just a visual check when an invalid number is entered that the invalid number is being received
+}
+
+for(let i = 1; i < 50; i++){
+    if(i === parseFloat(numberToSkip)){
+        console.log("Yikes! Skipping number: " + numberToSkip);
+        continue;
+    }
+    if(i % 2 === 0){
+        continue;
+    }
+    console.log("Here is an odd number: " + i);
+}
+})();
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,10 +1,10 @@
-"use strict";
+// (function(){
+// "use strict";
 
 // function showMultiplicationTable(x){
 //     for(var i = 1; i < 11; i++){
 //         console.log( x + " x " + i + " = " + (x*i));
 //     }
-//
 // }
 // showMultiplicationTable(prompt("Which number do you want a multiplication table for?"));
 
@@ -16,7 +16,7 @@
 //     else console.log(random + " is even");
 // }
 
-// found the Array().join() answer here https://stackoverflow.com/questions/1877475/repeat-a-string-in-javascript-a-number-of-times
+//found the Array().join() answer here https://stackoverflow.com/questions/1877475/repeat-a-string-in-javascript-a-number-of-times
 // for(var i = 1; i < 10; i++){
 //     console.log(Array(i+1).join(i));
 // }
@@ -24,3 +24,4 @@
 // for(var i = 100; i > 1; i-=5){
 //     console.log(i);
 // }
+//})();
