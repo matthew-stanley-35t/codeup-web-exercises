8ac9768d01f1aadb949a77e7920e7559792b4622
Matthew Stanley <matthew.stanley35t@gmail.com>
2022-10-17 10:52:30 -0600

    adding iterating_arrays_js.html and iterating.js for arrays exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,25 +1,25 @@
-(function(){
-"use strict";
-
-let numberToSkip = 0;
-
-while (numberToSkip !== undefined){
-    numberToSkip = prompt("Please enter an ODD number between 1 and 50");
-    if (numberToSkip % 2 !== 0 && numberToSkip > 0 && numberToSkip < 50){
-        console.log("Number to skip is: " + numberToSkip);
-        break;
-    }
-    console.log(numberToSkip); //just a visual check when an invalid number is entered that the invalid number is being received
-}
-
-for(let i = 1; i < 50; i++){
-    if(i === parseFloat(numberToSkip)){
-        console.log("Yikes! Skipping number: " + numberToSkip);
-        continue;
-    }
-    if(i % 2 === 0){
-        continue;
-    }
-    console.log("Here is an odd number: " + i);
-}
-})();
+// (function(){
+// "use strict";
+//
+// let numberToSkip = 0;
+//
+// while (numberToSkip !== undefined){
+//     numberToSkip = prompt("Please enter an ODD number between 1 and 50");
+//     if (numberToSkip % 2 !== 0 && numberToSkip > 0 && numberToSkip < 50){
+//         console.log("Number to skip is: " + numberToSkip);
+//         break;
+//     }
+//     console.log(numberToSkip); //just a visual check when an invalid number is entered that the invalid number is being received
+// }
+//
+// for(let i = 1; i < 50; i++){
+//     if(i === parseFloat(numberToSkip)){
+//         console.log("Yikes! Skipping number: " + numberToSkip);
+//         continue;
+//     }
+//     if(i % 2 === 0){
+//         continue;
+//     }
+//     console.log("Here is an odd number: " + i);
+// }
+// })();
