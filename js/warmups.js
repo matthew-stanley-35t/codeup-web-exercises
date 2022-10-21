5a42efe0b0c863ee3aa14e6378bed2eee2719abe
Matthew Stanley <matthew.stanley35t@gmail.com>
2022-10-21 08:32:54 -0600

    adding day 2s warmups to warmups.js

--- js/warmups.js
+++ js/warmups.js
@@ -1,3 +1,19 @@
 function howMuch(userObject){
     return userObject.price;
 }
+
+function getLongestString(arr){
+    let max_str = arr[0].length;
+    let longestString = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        let newMax = arr[i].length;
+        if (newMax > max_str) {
+            longestString = arr[i];
+            max_str = newMax;
+        }
+    }
+    return longestString;
+}
+
+// let sampleArr = ["the", "longest", "string"]
+// console.log(getLongestString(sampleArr));
