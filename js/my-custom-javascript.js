ecf2ee8a69d6d09f8ce6abccfa8067b532f48afc
Matthew Stanley <matthew.stanley35t@gmail.com>
2022-11-18 08:27:16 -0700

    adding jquery selectors exercises

---
+++ js/my-custom-javascript.js
@@ -0,0 +1,20 @@
+"use strict";
+
+$(function() {
+
+    // let contents = $('#codeup').html();
+    // alert(contents);
+    //
+    // $('.codeup').css('border', 'solid 1px red');
+
+    // $('li').css('font-size', '20px');
+    // $('h1').css('background-color', 'yellow');
+    // $('p').css('background-color', 'yellow');
+    // $('li').css('background-color', 'yellow');
+
+    // let headingAlert = $('h1').html();
+    // alert(headingAlert);
+
+    // $('h1, p, li').css('background-color', 'yellow')
+
+});
